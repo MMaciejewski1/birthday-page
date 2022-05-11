d1e8d5c39b8b1511929c0440d83343a47ec666ac
MMaciejewski1 <ramzes9496@gmail.com>
2022-05-11 20:36:48 +0200

    fix response
    add css for good look response and handle swipe for change page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+let x = 0;
 function showPages(id){
     var totalNumberOfPages = 5;
-    console.log('page'+id)
+    x=id;
     for(var i=0; i<=totalNumberOfPages; i++){
         if (document.getElementById('page'+i)) {
             document.getElementById('page'+i).style.display='none';
@@ -11,8 +12,29 @@ function showPages(id){
         }
 };
 document.onreadystatechange = function () {
-    var id = 0;
+    var id = x;
     showPages(id);
     id++;
-    setInterval(function() {showPages(id);(id<6)?id++:id=0;}, 10000);
+    setInterval(function() {showPages(id);(id<5)?id++:id=0;}, 10000);
+    const slider = document.getElementById('slider')
+    slider.addEventListener('touchstart', e => {
+        touchstartX = e.changedTouches[0].screenX
+      })
+      
+      slider.addEventListener('touchend', e => {
+        touchendX = e.changedTouches[0].screenX
+        handleGesture()
+      })
 }
+
+let touchstartX = 0
+let touchendX = 0
+
+
+function handleGesture() {
+  if (touchendX < touchstartX) (x==0)?x=5:x--;
+  if (touchendX > touchstartX) (x>5)?x=0:x++;
+  showPages(x);
+  console.log(x);
+}
+
